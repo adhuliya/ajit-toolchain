2fa03c36b5ba5d5971b42a3e312dd9966e03232e
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2020-10-26 11:26:19 +0530

    [vishnu] Added docker setup files.
    	Testing
    	- toolchain compiles succesfully
    	- Ajit linux fails at compiling device tree because of
    	  dtc installation failure; THIS NEEDS FIX

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,4 +6,5 @@ groupadd -g $GGID $GGROUP
 useradd  -u $GUID -s $GSHELL -c $GUSERNAME -g $GGID -M -d $GHOME $GUSERNAME
 usermod  -a -G sudo $GUSERNAME
 su -l $GUSERNAME    # become the current user and start a shell
+export PATH=$PATH:$HOME/bin
 #/bin/bash           # another root shell
