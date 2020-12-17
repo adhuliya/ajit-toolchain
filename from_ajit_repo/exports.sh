94f0dc604dbbfbf5d834c06dff2fea2a93e33c00
Saurabhb17 <saurabhbansode@gmail.com>
2020-12-17 12:45:05 +0530

    for access to flash image binaries

--- from_ajit_repo/exports.sh
+++ from_ajit_repo/exports.sh
@@ -8,6 +8,7 @@ export PATH=$ROOTDIR/tools/traceCompareServer/bin:$PATH
 export PATH=$ROOTDIR/tools/genVmapAsm/bin:$PATH
 export PATH=$ROOTDIR/tools/generateInitVal/bin:$PATH
 export PATH=$ROOTDIR/tools/vm/bin:$PATH
+export PATH=$ROOTDIR/tools/flash_image/bin/:$PATH
 
 #AHIR-related
 export LD_LIBRARY_PATH=$AHIR_RELEASE/functionLibrary/lib:$LD_LIBRARY_PATH
@@ -20,5 +21,6 @@ export PATH=$ROOTDIR/tools/linker:$PATH
 export PYTHONPATH=$ROOTDIR/tools/pyelftools-0.25:$PYTHONPATH
 export PYTHONPATH=$ROOTDIR/tools/scripts:$PYTHONPATH
 
-
+# $pwd/tools/cross-compiler/buildroot_cross_compiler/usr/bin/sparc-linux-readelf
+# $pwd/tools/cross-compiler/buildroot_cross_compiler/usr/bin/sparc-linux-as
 
