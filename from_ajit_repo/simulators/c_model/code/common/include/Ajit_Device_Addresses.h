a3aaf5137660c3e16b87e5643fd247187d913fa6
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2020-12-22 12:01:10 +0530

    [vishnu] corrected the memory range for sdhc.

--- from_ajit_repo/simulators/c_model/code/common/include/Ajit_Device_Addresses.h
+++ from_ajit_repo/simulators/c_model/code/common/include/Ajit_Device_Addresses.h
@@ -25,33 +25,33 @@
 
 //sdhc registers
 
-#define ADDR_SDHC_BASE_REGISTER                 0xFFFF3000
+#define ADDR_SDHC_BASE_REGISTER                 0xFFFF3300
 
-#define ADDR_SDHC_ARG_2                         0xFFFF3000      // 4 BYTES WIDE
-#define ADDR_SDHC_BLOCK_SIZE                    0xFFFF3004      // 2 BYTES WIDE
-#define ADDR_SDHC_BLOCK_COUNT                   0xFFFF3006      // 2 BYTES WIDE
-#define ADDR_SDHC_ARG_1                         0xFFFF3008      // 4 BYTES WIDE
-#define ADDR_SDHC_TRANSFER_MODE                 0xFFFF300C      // 2 BYTES WIDE
-#define ADDR_SDHC_REGISTER_COMMAND              0xFFFF300E      // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE                      0xFFFF3010      // 16 BYTES WIDE
-#define ADDR_SDHC_BUFFER_DATA_PORT              0xFFFF3020      // 4 BYTES WIDE
-#define ADDR_SDHC_PRESENT_STATE                 0xFFFF3024      // 4 BYTES WIDE
-#define ADDR_SDHC_HOST_CONTROL_1                0xFFFF3028      // 1 BYTES WIDE
-#define ADDR_SDHC_POWER_CONTROL                 0xFFFF3029      // 1 BYTES WIDE
-#define ADDR_SDHC_BLOCK_GAP_CONTROL             0xFFFF302A      // 1 BYTES WIDE
-#define ADDR_SDHC_WAKEUP_CONTROL                0xFFFF302B      // 1 BYTES WIDE
-#define ADDR_SDHC_CLOCK_CONTROL                 0xFFFF302C      // 2 BYTES WIDE
-#define ADDR_SDHC_TIMEOUT_CONTROL               0xFFFF302E      // 1 BYTES WIDE
-#define ADDR_SDHC_SOFTWARE_RESET                0xFFFF302F      // 1 BYTES WIDE
-#define ADDR_SDHC_NORMAL_INTR_STATUS            0xFFFF3030      // 2 BYTES WIDE
-#define ADDR_SDHC_ERROR_INTR_STATUS             0xFFFF3032      // 2 BYTES WIDE
-#define ADDR_SDHC_NORMAL_INTR_STATUS_EN         0xFFFF3034      // 2 BYTES WIDE
-#define ADDR_SDHC_ERROR_INTR_STATUS_EN          0xFFFF3036      // 2 BYTES WIDE
-#define ADDR_SDHC_NORMAL_INTR_SIGNAL_EN         0xFFFF3038      // 2 BYTES WIDE
-#define ADDR_SDHC_ERROR_INTR_SIGNAL_EN          0xFFFF303A      // 2 BYTES WIDE
-#define ADDR_SDHC_AUTO_CMD_ERROR_STATUS         0xFFFF303C      // 2 BYTES WIDE
-#define ADDR_SDHC_HOST_CONTROL_2                0xFFFF303E      // 2 BYTES WIDE
-#define ADDR_SDHC_CAPS                          0xFFFF3040      // 8 BYTES WIDE
-#define ADDR_SDHC_MAX_CURRENT_CAPS              0xFFFF3048      // 4 BYTES WIDE
-#define ADDR_SDHC_HOST_PRESET_VALUES            0xFFFF3060      // 16 BYTES WIDE
-#define ADDR_SDHC_HOST_CONTROLLER_VERSION       0xFFFF30FE      // 2 BYTES WIDE
+#define ADDR_SDHC_ARG_2                         0xFFFF3300      // 4 BYTES WIDE
+#define ADDR_SDHC_BLOCK_SIZE                    0xFFFF3304      // 2 BYTES WIDE
+#define ADDR_SDHC_BLOCK_COUNT                   0xFFFF3306      // 2 BYTES WIDE
+#define ADDR_SDHC_ARG_1                         0xFFFF3308      // 4 BYTES WIDE
+#define ADDR_SDHC_TRANSFER_MODE                 0xFFFF330C      // 2 BYTES WIDE
+#define ADDR_SDHC_REGISTER_COMMAND              0xFFFF330E      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE                      0xFFFF3310      // 16 BYTES WIDE
+#define ADDR_SDHC_BUFFER_DATA_PORT              0xFFFF3320      // 4 BYTES WIDE
+#define ADDR_SDHC_PRESENT_STATE                 0xFFFF3324      // 4 BYTES WIDE
+#define ADDR_SDHC_HOST_CONTROL_1                0xFFFF3328      // 1 BYTES WIDE
+#define ADDR_SDHC_POWER_CONTROL                 0xFFFF3329      // 1 BYTES WIDE
+#define ADDR_SDHC_BLOCK_GAP_CONTROL             0xFFFF332A      // 1 BYTES WIDE
+#define ADDR_SDHC_WAKEUP_CONTROL                0xFFFF332B      // 1 BYTES WIDE
+#define ADDR_SDHC_CLOCK_CONTROL                 0xFFFF332C      // 2 BYTES WIDE
+#define ADDR_SDHC_TIMEOUT_CONTROL               0xFFFF332E      // 1 BYTES WIDE
+#define ADDR_SDHC_SOFTWARE_RESET                0xFFFF332F      // 1 BYTES WIDE
+#define ADDR_SDHC_NORMAL_INTR_STATUS            0xFFFF3330      // 2 BYTES WIDE
+#define ADDR_SDHC_ERROR_INTR_STATUS             0xFFFF3332      // 2 BYTES WIDE
+#define ADDR_SDHC_NORMAL_INTR_STATUS_EN         0xFFFF3334      // 2 BYTES WIDE
+#define ADDR_SDHC_ERROR_INTR_STATUS_EN          0xFFFF3336      // 2 BYTES WIDE
+#define ADDR_SDHC_NORMAL_INTR_SIGNAL_EN         0xFFFF3338      // 2 BYTES WIDE
+#define ADDR_SDHC_ERROR_INTR_SIGNAL_EN          0xFFFF333A      // 2 BYTES WIDE
+#define ADDR_SDHC_AUTO_CMD_ERROR_STATUS         0xFFFF333C      // 2 BYTES WIDE
+#define ADDR_SDHC_HOST_CONTROL_2                0xFFFF333E      // 2 BYTES WIDE
+#define ADDR_SDHC_CAPS                          0xFFFF3340      // 8 BYTES WIDE
+#define ADDR_SDHC_MAX_CURRENT_CAPS              0xFFFF3348      // 4 BYTES WIDE
+#define ADDR_SDHC_HOST_PRESET_VALUES            0xFFFF3360      // 16 BYTES WIDE
+#define ADDR_SDHC_HOST_CONTROLLER_VERSION       0xFFFF33FE      // 2 BYTES WIDE
