cafa51f9c0febd390ac257409a90010da9133321
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2020-12-28 11:12:13 +0530

    [vishnu] Added action outline for regs. Added a blueprint for using switch-case instead 	 of if-else ladder.

--- from_ajit_repo/simulators/c_model/code/common/include/Ajit_Device_Addresses.h
+++ from_ajit_repo/simulators/c_model/code/common/include/Ajit_Device_Addresses.h
@@ -23,51 +23,52 @@
 #define ADDR_SERIAL_RX_REGISTER      0xFFFF3220
 
 
-//sdhc registers
+// sdhc registers; actual values range from 0xFFFF3300 - 0xFFFF33FF
+// values modified for easy processing as 24 bits. 
 
 
 
-#define ADDR_SDHC_ARG_2                         0xFFFF3300      // 4 BYTES WIDE
-#define ADDR_SDHC_BLOCK_SIZE                    0xFFFF3304      // 2 BYTES WIDE
-#define ADDR_SDHC_BLOCK_COUNT                   0xFFFF3306      // 2 BYTES WIDE
-#define ADDR_SDHC_ARG_1                         0xFFFF3308      // 4 BYTES WIDE
-#define ADDR_SDHC_TRANSFER_MODE                 0xFFFF330C      // 2 BYTES WIDE
-#define ADDR_SDHC_REGISTER_COMMAND              0xFFFF330E      // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE0                     0xFFFF3310     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE1                     0xFFFF3312     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE2                     0xFFFF3314     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE3                     0xFFFF3316     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE4                     0xFFFF3318     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE5                     0xFFFF331A     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE6                     0xFFFF331C     // 2 BYTES WIDE
-#define ADDR_SDHC_RESPONSE7                     0xFFFF331E     // 2 BYTES WIDE
-#define ADDR_SDHC_BUFFER_DATA_PORT              0xFFFF3320      // 4 BYTES WIDE
-#define ADDR_SDHC_PRESENT_STATE                 0xFFFF3324      // 4 BYTES WIDE
-#define ADDR_SDHC_POWER_CONTROL                 0xFFFF3328      // 1 BYTES WIDE
-#define ADDR_SDHC_HOST_CONTROL_1                0xFFFF3329      // 1 BYTES WIDE
-#define ADDR_SDHC_WAKEUP_CONTROL                0xFFFF332A      // 1 BYTES WIDE
-#define ADDR_SDHC_BLOCK_GAP_CONTROL             0xFFFF332B      // 1 BYTES WIDE
-#define ADDR_SDHC_CLOCK_CONTROL                 0xFFFF332C      // 2 BYTES WIDE
-#define ADDR_SDHC_SOFTWARE_RESET                0xFFFF332E      // 1 BYTES WIDE
-#define ADDR_SDHC_TIMEOUT_CONTROL               0xFFFF332F      // 1 BYTES WIDE
-#define ADDR_SDHC_NORMAL_INTR_STATUS            0xFFFF3330      // 2 BYTES WIDE
-#define ADDR_SDHC_ERROR_INTR_STATUS             0xFFFF3332      // 2 BYTES WIDE
-#define ADDR_SDHC_NORMAL_INTR_STATUS_EN         0xFFFF3334      // 2 BYTES WIDE
-#define ADDR_SDHC_ERROR_INTR_STATUS_EN          0xFFFF3336      // 2 BYTES WIDE
-#define ADDR_SDHC_NORMAL_INTR_SIGNAL_EN         0xFFFF3338      // 2 BYTES WIDE
-#define ADDR_SDHC_ERROR_INTR_SIGNAL_EN          0xFFFF333A      // 2 BYTES WIDE
-#define ADDR_SDHC_AUTO_CMD_ERROR_STATUS         0xFFFF333C      // 2 BYTES WIDE
-#define ADDR_SDHC_HOST_CONTROL_2                0xFFFF333E      // 2 BYTES WIDE
-#define ADDR_SDHC_CAPS                          0xFFFF3340      // 8 BYTES WIDE
-#define ADDR_SDHC_MAX_CURRENT_CAPS              0xFFFF3348      // 4 BYTES WIDE
-#define ADDR_SDHC_MAX_CURRENT_CAPS_RES          0xFFFF334C      // 4 BYTES WIDE
-#define ADDR_SDHC_FORCE_EVENT_AUTOCMD_ERRSTAT   0xFFFF3350      // 2 BYTES WIDE
-#define ADDR_SDHC_FORCE_EVENT_AUTOCMD_ERRSTAT_R 0xFFFF3352      // 2 BYTES WIDE
-#define ADDR_SDHC_ADMA_ERR_STAT                 0xFFFF3355      // 1 BYTE WIDE
-#define ADDR_SDHC_ADMA_SYSTEM_ADDR              0xFFFF3358      // 8 BYTES WIDE
-#define ADDR_SDHC_HOST_PRESET_VALUES            0xFFFF3360      // 16 BYTES WIDE
-#define ADDR_SDHC_SHARED_BUS_CTRL               0xFFFF33E0      //2 BYTES WIDE
-#define ADDR_SDHC_SLOT_INTR_STATUS              0xFFFF33FC      //2 BYTES WIDE
-#define ADDR_SDHC_HOST_CONTROLLER_VERSION       0xFFFF33FE      // 2 BYTES WIDE
+#define ADDR_SDHC_ARG_2                         0xFF3300      // 4 BYTES WIDE
+#define ADDR_SDHC_BLOCK_SIZE                    0xFF3304      // 2 BYTES WIDE
+#define ADDR_SDHC_BLOCK_COUNT                   0xFF3306      // 2 BYTES WIDE
+#define ADDR_SDHC_ARG_1                         0xFF3308      // 4 BYTES WIDE
+#define ADDR_SDHC_TRANSFER_MODE                 0xFF330C      // 2 BYTES WIDE
+#define ADDR_SDHC_REGISTER_COMMAND              0xFF330E      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE0                     0xFF3310      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE1                     0xFF3312      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE2                     0xFF3314      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE3                     0xFF3316      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE4                     0xFF3318      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE5                     0xFF331A      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE6                     0xFF331C      // 2 BYTES WIDE
+#define ADDR_SDHC_RESPONSE7                     0xFF331E      // 2 BYTES WIDE
+#define ADDR_SDHC_BUFFER_DATA_PORT              0xFF3320      // 4 BYTES WIDE
+#define ADDR_SDHC_PRESENT_STATE                 0xFF3324      // 4 BYTES WIDE
+#define ADDR_SDHC_POWER_CONTROL                 0xFF3328      // 1 BYTES WIDE
+#define ADDR_SDHC_HOST_CONTROL_1                0xFF3329      // 1 BYTES WIDE
+#define ADDR_SDHC_WAKEUP_CONTROL                0xFF332A      // 1 BYTES WIDE
+#define ADDR_SDHC_BLOCK_GAP_CONTROL             0xFF332B      // 1 BYTES WIDE
+#define ADDR_SDHC_CLOCK_CONTROL                 0xFF332C      // 2 BYTES WIDE
+#define ADDR_SDHC_SOFTWARE_RESET                0xFF332E      // 1 BYTES WIDE
+#define ADDR_SDHC_TIMEOUT_CONTROL               0xFF332F      // 1 BYTES WIDE
+#define ADDR_SDHC_NORMAL_INTR_STATUS            0xFF3330      // 2 BYTES WIDE
+#define ADDR_SDHC_ERROR_INTR_STATUS             0xFF3332      // 2 BYTES WIDE
+#define ADDR_SDHC_NORMAL_INTR_STATUS_EN         0xFF3334      // 2 BYTES WIDE
+#define ADDR_SDHC_ERROR_INTR_STATUS_EN          0xFF3336      // 2 BYTES WIDE
+#define ADDR_SDHC_NORMAL_INTR_SIGNAL_EN         0xFF3338      // 2 BYTES WIDE
+#define ADDR_SDHC_ERROR_INTR_SIGNAL_EN          0xFF333A      // 2 BYTES WIDE
+#define ADDR_SDHC_AUTO_CMD_ERROR_STATUS         0xFF333C      // 2 BYTES WIDE
+#define ADDR_SDHC_HOST_CONTROL_2                0xFF333E      // 2 BYTES WIDE
+#define ADDR_SDHC_CAPS                          0xFF3340      // 8 BYTES WIDE
+#define ADDR_SDHC_MAX_CURRENT_CAPS              0xFF3348      // 4 BYTES WIDE
+#define ADDR_SDHC_MAX_CURRENT_CAPS_RES          0xFF334C      // 4 BYTES WIDE
+#define ADDR_SDHC_FORCE_EVENT_AUTOCMD_ERRSTAT   0xFF3350      // 2 BYTES WIDE
+#define ADDR_SDHC_FORCE_EVENT_AUTOCMD_ERRSTAT_R 0xFF3352      // 2 BYTES WIDE
+#define ADDR_SDHC_ADMA_ERR_STAT                 0xFF3355      // 1 BYTE WIDE
+#define ADDR_SDHC_ADMA_SYSTEM_ADDR              0xFF3358      // 8 BYTES WIDE
+#define ADDR_SDHC_HOST_PRESET_VALUES            0xFF3360      // 16 BYTES WIDE
+#define ADDR_SDHC_SHARED_BUS_CTRL               0xFF33E0      // 2 BYTES WIDE
+#define ADDR_SDHC_SLOT_INTR_STATUS              0xFF33FC      // 2 BYTES WIDE
+#define ADDR_SDHC_HOST_CONTROLLER_VERSION       0xFF33FE      // 2 BYTES WIDE
 
 
--- from_ajit_repo/simulators/c_model/code/devices/src/sdhc.c
+++ from_ajit_repo/simulators/c_model/code/devices/src/sdhc.c
@@ -36,6 +36,8 @@ uint32_t sdhc_tx_buffer;
 uint32_t sdhc_rx_buffer;
 uint8_t SDHC_INT_OUT;
 
+SDHCInternalMap internal_reg;
+
 // **** Thread declarations ******//
 void SDHC_CPU_Control();
 DEFINE_THREAD(SDHC_CPU_Control);
@@ -84,80 +86,165 @@ void updateRegister(uint32_t data_in, uint32_t addr, uint8_t byte_mask,
  struct CPUViewOfSDHCRegs *str, struct SDHCInternalMap *int_str)
 {
 	uint32_t data_in_masked = insertUsingByteMask(0, data_in, byte_mask);
+	
+	// we can replace the switch ladder with the following switch-case format.
+	// NOTE: I modified the address defines in ajit_device_addresses for easy processing.
+	// uint32_t addr_in = addr;
+
+	// switch (addr_in)
+	// {
+	// case ADDR_SDHC_ARG_2:
+
+	// 	uint8_t temp1 = getSlice32(data_in_masked,7,0);
+	// 	str->argument2[0] = temp1;
+	// 	uint8_t temp2 = getSlice32(data_in_masked,15,8);
+	// 	str->argument2[1] = temp2;
+	// 	uint8_t temp3 = getSlice32(data_in_masked,23,16);
+	// 	str->argument2[2] = temp3;
+	// 	uint8_t temp4 = getSlice32(data_in_masked,31,24);
+	// 	str->argument2[3] = temp4;
+	// 	uint8_t size=4;
+	// 	void *dest = &(int_str->argument2);
+	// 	void *source = &(str->argument2);
+	// 	memcpy(dest,source,size);
+		
+	// 	break;
+	
+	// case ADDR_SDHC_BLOCK_SIZE:
+
+	// 	uint8_t temp1 = getSlice32(data_in_masked,7,0);
+	// 	str->blk_size[0] = temp1;
+	// 	uint8_t temp2 = getSlice32(data_in_masked,15,8);
+	// 	str->blk_size[1] = temp2;
+	// 	uint8_t size=2;
+	// 	void *dest = &(int_str->blk_size);
+	// 	void *source = &(str->blk_size);
+	// 	memcpy(dest,source,size);
+
+	// 	break;
+
+	// case ADDR_SDHC_BLOCK_COUNT:
+		
+	// 	uint8_t temp1 = getSlice32(data_in_masked,7,0);
+	// 	str->blk_count[0] = temp1;
+	// 	uint8_t temp2 = getSlice32(data_in_masked,15,8);
+	// 	str->blk_count[1] = temp2;
+	// 	uint8_t size=2;
+	// 	void *dest = &(int_str->blk_count);
+	// 	void *source = &(str->blk_count);
+	// 	memcpy(dest,source,size);
+
+	// 	break;
+
+	// case ADDR_SDHC_ARG_1:
+
+	// 	uint8_t temp1 = getSlice32(data_in_masked,7,0);
+	// 	str->argument1[0] = temp1;
+	// 	uint8_t temp2 = getSlice32(data_in_masked,15,8);
+	// 	str->argument1[1] = temp2;
+	// 	uint8_t temp3 = getSlice32(data_in_masked,23,16);
+	// 	str->argument1[2] = temp3;
+	// 	uint8_t temp4 = getSlice32(data_in_masked,31,24);
+	// 	str->argument1[3] = temp4;
+	// 	uint8_t size=4;
+	// 	void *dest = &(int_str->argument1);
+	// 	void *source = &(str->argument1);
+	// 	memcpy(dest,source,size);
+
+	// 	break;
+
+	// case ADDR_SDHC_TRANSFER_MODE:
+
+	// 	uint8_t temp1 = getSlice32(data_in_masked,7,0);
+	// 	str->tx_mode[0] = temp1;
+	// 	uint8_t temp2 = getSlice32(data_in_masked,15,8);
+	// 	str->tx_mode[1] = temp2;
+	// 	uint8_t size=2;
+	// 	void *dest = &(int_str->tx_mode);
+	// 	void *source = &(str->tx_mode);
+	// 	memcpy(dest,source,size);
+
+	// 	break;
+
+	// default:
+	// 	break;
+	// }
+
 	if (addr== (0xffffff & ADDR_SDHC_ARG_2))
 	{
-	uint8_t temp1 = getSlice32(data_in_masked,7,0);
-	str->argument2[0] = temp1;
-	uint8_t temp2 = getSlice32(data_in_masked,15,8);
-	str->argument2[1] = temp2;
-	uint8_t temp3 = getSlice32(data_in_masked,23,16);
-	str->argument2[2] = temp3;
-	uint8_t temp4 = getSlice32(data_in_masked,31,24);
-	str->argument2[3] = temp4;
-	uint8_t size=4;
-	void *dest = &(int_str->argument2);
-	void *source = &(str->argument2);
-	memcpy(dest,source,size);
+		uint8_t temp1 = getSlice32(data_in_masked,7,0);
+		str->argument2[0] = temp1;
+		uint8_t temp2 = getSlice32(data_in_masked,15,8);
+		str->argument2[1] = temp2;
+		uint8_t temp3 = getSlice32(data_in_masked,23,16);
+		str->argument2[2] = temp3;
+		uint8_t temp4 = getSlice32(data_in_masked,31,24);
+		str->argument2[3] = temp4;
+		uint8_t size=4;
+		void *dest = &(int_str->argument2);
+		void *source = &(str->argument2);
+		memcpy(dest,source,size);
 	}
 
 	else if (addr== (0xffffff & ADDR_SDHC_BLOCK_SIZE))
 	{
-	uint8_t temp1 = getSlice32(data_in_masked,7,0);
-	str->blk_size[0] = temp1;
-	uint8_t temp2 = getSlice32(data_in_masked,15,8);
-	str->blk_size[1] = temp2;
-	uint8_t size=2;
-	void *dest = &(int_str->blk_size);
-	void *source = &(str->blk_size);
-	memcpy(dest,source,size);
+		uint8_t temp1 = getSlice32(data_in_masked,7,0);
+		str->blk_size[0] = temp1;
+		uint8_t temp2 = getSlice32(data_in_masked,15,8);
+		str->blk_size[1] = temp2;
+		uint8_t size=2;
+		void *dest = &(int_str->blk_size);
+		void *source = &(str->blk_size);
+		memcpy(dest,source,size);
 	}
 
 	else if (addr== (0xffffff & ADDR_SDHC_BLOCK_COUNT))
 	{
-	uint8_t temp1 = getSlice32(data_in_masked,7,0);
-	str->blk_count[0] = temp1;
-	uint8_t temp2 = getSlice32(data_in_masked,15,8);
-	str->blk_count[1] = temp2;
-	uint8_t size=2;
-	void *dest = &(int_str->blk_count);
-	void *source = &(str->blk_count);
-	memcpy(dest,source,size);
+		uint8_t temp1 = getSlice32(data_in_masked,7,0);
+		str->blk_count[0] = temp1;
+		uint8_t temp2 = getSlice32(data_in_masked,15,8);
+		str->blk_count[1] = temp2;
+		uint8_t size=2;
+		void *dest = &(int_str->blk_count);
+		void *source = &(str->blk_count);
+		memcpy(dest,source,size);
 	}
 
 	else 	if (addr== (0xffffff & ADDR_SDHC_ARG_1))
 	{
-	uint8_t temp1 = getSlice32(data_in_masked,7,0);
-	str->argument1[0] = temp1;
-	uint8_t temp2 = getSlice32(data_in_masked,15,8);
-	str->argument1[1] = temp2;
-	uint8_t temp3 = getSlice32(data_in_masked,23,16);
-	str->argument1[2] = temp3;
-	uint8_t temp4 = getSlice32(data_in_masked,31,24);
-	str->argument1[3] = temp4;
-	uint8_t size=4;
-	void *dest = &(int_str->argument1);
-	void *source = &(str->argument1);
-	memcpy(dest,source,size);
+		uint8_t temp1 = getSlice32(data_in_masked,7,0);
+		str->argument1[0] = temp1;
+		uint8_t temp2 = getSlice32(data_in_masked,15,8);
+		str->argument1[1] = temp2;
+		uint8_t temp3 = getSlice32(data_in_masked,23,16);
+		str->argument1[2] = temp3;
+		uint8_t temp4 = getSlice32(data_in_masked,31,24);
+		str->argument1[3] = temp4;
+		uint8_t size=4;
+		void *dest = &(int_str->argument1);
+		void *source = &(str->argument1);
+		memcpy(dest,source,size);
 	}
 
 	else if(addr == (0xffffff & ADDR_SDHC_TRANSFER_MODE))
 	{
-	uint8_t temp1 = getSlice32(data_in_masked,7,0);
-	str->tx_mode[0] = temp1;
-	uint8_t temp2 = getSlice32(data_in_masked,15,8);
-	str->tx_mode[1] = temp2;
-	uint8_t size=2;
-	void *dest = &(int_str->tx_mode);
-	void *source = &(str->tx_mode);
-	memcpy(dest,source,size);
+		uint8_t temp1 = getSlice32(data_in_masked,7,0);
+		str->tx_mode[0] = temp1;
+		uint8_t temp2 = getSlice32(data_in_masked,15,8);
+		str->tx_mode[1] = temp2;
+		uint8_t size=2;
+		void *dest = &(int_str->tx_mode);
+		void *source = &(str->tx_mode);
+		memcpy(dest,source,size);
 	}
 //to be used soon:	sdhc_tx_buffer = 1;//load the updated value in a pipe
 }
 
 void SDHC_CPU_Control()
 {
-struct CPUViewOfSDHCRegs cpu_reg_view;
-struct SDHCInternalMap internal_map;
+	struct CPUViewOfSDHCRegs cpu_reg_view;
+	struct SDHCInternalMap internal_map;
+	
 	while(1)
 	{
 		uint8_t rwbar, byte_mask;
@@ -170,25 +257,163 @@ struct SDHCInternalMap internal_map;
 		if(!rwbar)
 		{
 			//***lock the state variables***
-		pthread_mutex_lock(&Sdhc_lock);	
+			pthread_mutex_lock(&Sdhc_lock);	
 			
 			if((addr<=ADDR_SDHC_ARG_2)&&(addr>=ADDR_SDHC_HOST_CONTROLLER_VERSION))
 			{
 				updateRegister(data_in, addr, byte_mask, &cpu_reg_view, &internal_map);
 			}
-		pthread_mutex_unlock(&Sdhc_lock);	
+			pthread_mutex_unlock(&Sdhc_lock);	
 		}
 		else
 		{
-		pthread_mutex_lock(&Sdhc_lock);
+			pthread_mutex_lock(&Sdhc_lock);
 			if((addr<=ADDR_SDHC_ARG_2)&&(addr>=ADDR_SDHC_HOST_CONTROLLER_VERSION))
 			{
-//function for reading register values
+				//function for reading register values
 			}
-		pthread_mutex_unlock(&Sdhc_lock);	
+			pthread_mutex_unlock(&Sdhc_lock);	
 		}
 		
 	}
 }
 
+int action_reg_arg2()
+{
+	// RW
+	// used with Auto CMD23 to set a 32-bit blk count value
+}
+int action_reg_blksize()
+{
+	// Rsvd and RW
+	// one portion or sdma
+	// blk size of data ttrnafers for cmd17, cmd18, cmd24, cmd25 and cmd53
+}
+int action_reg_blkcount()
+{
+	// RW
+	// enabled when 'block count enable' in 'transfer mode' is set to 1
+	// valid for only multiple block transfers
+	// host driver set this value and host controller decrements the value
+	// according to the tranfered blocks; stops when reaches zero
+	// can be accessed onlywhen no transactions is executing; during data transfer
+	// ignore write and read will be incorrect
+}
+int action_reg_arg1()
+{
+	// RW
+}
+int action_reg_txmode()
+{
+	// Rsvd and RW
+	// used to control the operation of data transfer
+	// to prevent data loss host controller shall implement write protection during
+	// data transfers; writes to this reg sahll be ignroes when 'comand inhibit (DAT)'
+	// in 'present state reg' is 1
+}
+int action_reg_cmd()
+{
+	// host driver shall check the 'command inhibit (DAT)' and 'vommand inhibit (CMD)'
+	// bits in 'present state' reg beforewriting to thid reg
+	// writing to the upper byte triggersSD command generation
+	// host controller doesn't protect for writing when 'command inhibit(CMD)' is set
+}
+int action_reg_rsp0();
+int action_reg_rsp1();
+int action_reg_rsp2();
+int action_reg_rsp3();
+int action_reg_rsp4();
+int action_reg_rsp5();
+int action_reg_rsp6();
+int action_reg_rsp7();
+int action_reg_buffr_data_port()
+{
+	// RW
+	// foraccessing host controller buffer
+}
+int action_reg_psr()
+{
+	// Rsvvd, RO and ROC
+	// host driver can get the status of the host controller
+	// from this 32-bit read-only reg
+}
+int action_reg_pwr_ctrl();
+int action_reg_host_ctrl();
+int action_reg_wakeup_ctrl();
+int action_reg_blk_gap_ctrl();
+int action_reg_clk_ctrl();
+int action_reg_sw_rst()
+{
+	// write 1 to all the unrsvd bits for resetting
+	// after completing reset clear all bits
+	// 
+	// affects the following regs
+	// 	buffer data port reg
+	// 	Present state reg
+	// 	blk gap ctrl reg
+	// 	normal intr status reg
+	// 	
+}
+int action_reg_timeout_ctrl();
+int action_reg_nrml_itr_status();
+int action_reg_err_itr_status();
+int action_reg_nrml_itr_status_en();
+int action_reg_err_itr_status_en();
+int action_reg_nrml_itr_signal_en();
+int action_reg_err_itr_signal_en();
+int action_reg_acmd_err_status();
+int action_reg_host_ctrl2();
+int action_reg_cap()
+{
+	// whatever the case, values/states wont change
+	// writes are ignored
+	return -1;
+}
+int action_reg_max_current_cap()
+{
+	// whatever the case, values/states wont change
+	// writes are ignored
+	return -1;
+}
+int action_reg_force_event_autoCMD_err_stat();
+int action_reg_force_event_autoCMD_err_interrupt_stat();
+int action_reg_adma_err_status()
+{
+	// DMA not present
+	return -1;
+}
+int action_reg_adma_sys_addr()
+{
+	// DMA not present
+	return -1;
+}
+int action_reg_preset_value();
+int action_reg_shared_bus_ctrl()
+{
+	// optional
+	// relavent when shared bus is used
+	return -1;
+}
+int action_reg_slot_itr()
+{
+	// ROC
+	// writes are ignored
+	// relavent nly when there are slots
+	return 0;
+}
+int action_reg_host_controller_version()
+{
+	// whatever the case, values/states wont change
+	// writes are ignored
+	return -1;	
+}
 
+void reg_set(uint32_t addr)
+{
+	// logic to set 1 as value for the mentioned reg
+}
+
+void reg_clear(uint32_t addr)
+{
+	// logic to set 0 as the value in the reg passed on
+}
