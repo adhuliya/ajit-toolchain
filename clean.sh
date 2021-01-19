c427ca6008f89d769e4b1a4f0b35b613f3b94350
anshuman dhuliya <dhuliya@cse.iitb.ac.in>
2021-01-20 03:30:02 +0530

    better clean.sh

--- clean.sh
+++ clean.sh
@@ -2,6 +2,18 @@
 
 rm -Rf Dockerfile;
 rm -Rf build/;
+rm -Rf docker_build.log;
+rm -Rf build.log;
+
+if [[ -z $AJIT_CMODEL_DIR ]]; then
+  echo "ERROR: Please set the env variable \$AJIT_CMODEL_DIR";
+  exit 1;
+fi
+
+if [[ -z $AJIT_TOOLS_DIR]]; then
+  echo "ERROR: Please set the env variable \$AJIT_TOOLS_DIR";
+  exit 1;
+fi
 
 cd $AJIT_CMODEL_DIR;
 ./clean.sh;
