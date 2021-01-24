da741b48c948d6f41469506bdd79cb4a587a6da0
Saurabhb17 <saurabhbansode@gmail.com>
2021-01-24 13:22:22 +0530

    Archive added

---
+++ os/Ajit_linux/Archive/Ajit_bootloader_load_from_serial_port/run.sh
@@ -0,0 +1,1 @@
+./bootloader_frontend.py -m sample_mem_map.txt | AJIT_cpu_cache_mmu_testbench_vanilla Ajit_bootloader.mmap
---
+++ os/Ajit_linux/Archive/initramfs/Minimal_initramfs/init_executable.c
@@ -0,0 +1,13 @@
+//Minimal init
+
+#include<stdio.h>
+
+int main()
+{
+	printf("\n");
+	printf("\n Hello World From init() process! \n");
+	printf("\n entering an infinite loop ... ");
+	printf("\n");
+	while(1) { }
+	return 0;
+}
