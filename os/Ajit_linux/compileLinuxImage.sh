b7e9ad8b415c08648727d282cf67da3c57471182
Saurabhb17 <saurabhbansode@gmail.com>
2021-01-24 14:25:51 +0530

    some minor changes

--- os/Ajit_linux/compileLinuxImage.sh
+++ os/Ajit_linux/compileLinuxImage.sh
@@ -1,13 +1,13 @@
 # Script for producing Linux image for AJIT processor
 
 echo "================================================="
-echo "Executing Step 1 of AJIT Linux build, log can be found in compilelog/ directory"
-./GeneratememMapForAjit.sh |& tee ./compilelog/GenMemMapScript.log
+echo "Executing Step 1 of AJIT Linux build, log can be found in build-logs/ directory"
+./GeneratememMapForAjit.sh |& tee ./build-logs/GenMemMapScript.log
 echo "Finished Step 1 of AJIT Linux build"
 
 echo "================================================="
-echo "Executing Step 2 of AJIT Linux build, log can be found in compilelog/ directory"
-./vmLinuzToMemmapAtf0004000.sh |& tee ./compilelog/vmLinuzF0004000.log
+echo "Executing Step 2 of AJIT Linux build, log can be found in build-logs/ directory"
+./vmLinuzToMemmapAtf0004000.sh |& tee ./build-logs/vmLinuzF0004000.log
 echo "Finished Step 2 of AJIT Linux build"
 
 cp Ajit_linux/Generated_memory_map.f0004000/Output.f0004000/boot_loader_plus_kernel.mmap ./Ajit_linux/linux_boot/boot_loader_plus_kernel_mmap/
