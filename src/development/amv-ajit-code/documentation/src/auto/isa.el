f1ced80a145f9c8433903a6cdffab7a4db49059c
Abhijat Vichare <abhijatv@gmail.com>
2021-02-01 15:33:38 +0530

    Updated documentation of the SPARC based AJIT implementation.

---
+++ src/development/amv-ajit-code/documentation/src/auto/isa.el
@@ -0,0 +1,36 @@
+(TeX-add-style-hook
+ "isa"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../src/isa-v2-details"
+    "../src/binutils"
+    "../src/sisad"
+    "../src/packaging"
+    "book"
+    "bk10"
+    "epsf"
+    "pstricks"
+    "enumitem"
+    "fullpage"
+    "hyperref")
+   (TeX-add-symbols
+    "note"
+    "attention")
+   (LaTeX-add-labels
+    "chap:from:mpd:at:iitb"
+    "sec:Overview"
+    "sec:changes:wrt:v1"
+    "sec:insn:modified"
+    "sec:asr:mappings"
+    "sec:isa:extns"
+    "sec:isa:v2"))
+ :latex)
+
---
+++ src/development/amv-ajit-code/documentation/src/auto/shift-insn-extns.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "shift-insn-extns"
+ (lambda ()
+   (LaTeX-add-labels
+    "sec:shift:insn:impl"
+    "fig:ajit:slld:insn"
+    "fig:ajit:srld:insn"
+    "fig:ajit:srad:insn"
+    "fig:ajit:slldcc:insn"
+    "fig:ajit:srldcc:insn"))
+ :latex)
+
