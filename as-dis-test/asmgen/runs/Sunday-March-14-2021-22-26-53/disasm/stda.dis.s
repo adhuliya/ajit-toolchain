18ebe4d8147e65cf2b99c80fba68d55bb227453c
Abhijat Vichare <abhijatv@gmail.com>
2021-03-15 15:55:37 +0530

    Static AJIT tests: Removed unnecessary directory.

--- as-dis-test/asmgen/runs/Sunday-March-14-2021-22-26-53/disasm/stda.dis.s
+++
@@ -1,9 +0,0 @@
-
-/lindata/My-work/Professional/iitb/development/isa-v2-branch/ajit-toolchain/as-dis-test/asmgen/runs/Sunday-March-14-2021-22-26-53/objs/stda.o:     file format elf32-sparc
-
-
-Disassembly of section .text:
-
-00000000 <main>:
-   0:	01 00 00 00 	nop 
-   4:	01 00 00 00 	nop 
