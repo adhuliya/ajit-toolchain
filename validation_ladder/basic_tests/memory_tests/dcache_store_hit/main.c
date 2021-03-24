1ec70200bd6061ca184e773d9f923beb29161783
Saurabhb17 <saurabhbansode@gmail.com>
2021-03-24 11:25:58 +0530

    DCACHE Store cases

---
+++ validation_ladder/basic_tests/memory_tests/dcache_store_hit/main.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include "ajit_access_routines.h"
+#include "core_portme.h"
+
+int init_src_array();
+int run_test(int length, double* t);
+
+int main()
+{
+	__ajit_write_serial_control_register__ ( TX_ENABLE | RX_ENABLE);
+
+	ee_printf("Starting by filling up the array in cache lines\n");
+
+	init_src_array();
+	ee_printf("Done initialising array\n");
+	int err = 0;
+	int i;
+	double t;
+	err = run_test (256, &t);
+	ee_printf("For 256 stores, time elapsed is t=%f seconds, \n and err=%d\n", t, err);
+	ee_printf("Done\n");
+
+	return(1);
+}
