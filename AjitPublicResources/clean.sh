d230f6bfda5efb1c19628757030409133691eb99
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-04-29 06:29:58 +0530

    Public stuff kept in public.

--- AjitPublicResources/clean.sh
+++ AjitPublicResources/clean.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-rm -rf tools
-rm -rf processor/64bit/*
-rm -rf processor/TestEnvironments
+CCWD=$(pwd)
+# build test-environments
+cd processor/TestEnvironments
+scons -c 
+cd -
+# the 64-bit C reference model testbench.
+cd processor/64bit/C_multi_core_multi_thread/
+scons -c
+cd -
+# ajit_debug_monitor_mt.
+cd tools/ajit_debug_monitor_mt
+scons -c
+cd $CCWD
