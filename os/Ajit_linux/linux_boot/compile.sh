47b390dc75f38f237d0136f93d1c02a444eb22ed
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2021-05-27 23:01:21 +0530

    [vishnu]- explanded memory range for larger linux binary

--- os/Ajit_linux/linux_boot/compile.sh
+++ os/Ajit_linux/linux_boot/compile.sh
@@ -1,8 +1,11 @@
+#copy the mmap
+cp ../Generated_memory_map.f0004000/Output.f0004000/boot_loader_plus_kernel.mmap ./boot_loader_plus_kernel_mmap/ 
+
 #  Memory map file for OS + bootloader.
 # generate mmap.remapped file for application.
 cd flash_contents/
 # generate assembly code for mmap..
-mmapToAssyU64 ../boot_loader_plus_kernel_mmap/boot_loader_plus_kernel.mmap boot_loader_plus_kernel.s  0xf0000000 0x00200000
+mmapToAssyU64 ../boot_loader_plus_kernel_mmap/boot_loader_plus_kernel.mmap boot_loader_plus_kernel.s  0xf0000000 0x00600000
 # produces mmap file for bootstrap.
 ./compile_for_ajit.sh
 mmapToBin  24  bootstrap_u64.mmap bootstrap_u64.bin 
