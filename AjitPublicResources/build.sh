7244c11d909ad7847f287f6b32dd106f7e0f9f23
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-06-26 17:41:37 +0530

    Updated.

--- AjitPublicResources/build.sh
+++ AjitPublicResources/build.sh
@@ -11,17 +11,17 @@ cd processor/TestEnvironments
 scons
 cd $CWD
 # the C reference model testbench.
-#source reference_C_model_exports.sh
-#cd processor/C_reference_model/
-#echo "build processor/C_reference_model"
-#scons
-#cd -
+source reference_C_model_exports.sh
+cd processor/C_reference_model/
+echo "build processor/C_reference_model"
+scons
+cd -
 # the 64-bit C reference model testbench.
-#source reference_64bit_C_model_exports.sh
-#cd processor/64bit/C_multi_core_multi_thread/
-#echo "build processor/64bit/C_multi_core_multi_thread/"
-#scons
-#cd $CWD
+source reference_64bit_C_model_exports.sh
+cd processor/64bit/C_multi_core_multi_thread/
+echo "build processor/64bit/C_multi_core_multi_thread/"
+scons
+cd $CWD
 # ajit_debug_monitor_mt.
 echo "build tools/ajit_debug_monitor_mt"
 cd tools/ajit_debug_monitor_mt
