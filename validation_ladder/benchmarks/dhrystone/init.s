4ed556983825eab79a8dfd1f7a6915694ca58d8f
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-07-02 17:46:49 +0530

    Updated.

--- validation_ladder/benchmarks/dhrystone/init.s
+++ validation_ladder/benchmarks/dhrystone/init.s
@@ -2,6 +2,10 @@
 main:
 _start:
 	mov %g0, %sp
+	! mmu control register format
+	!   [8]                    [7:1]               [0]
+        !   default-cacheable bit, unused,		mmu-enable
+	!
 	set 0x100, %o0
 	sta %o0, [%g0] 0x4      ! set the default cacheable bit in the mmu to 1. mmu stays disabled.
 	call ajit_main
