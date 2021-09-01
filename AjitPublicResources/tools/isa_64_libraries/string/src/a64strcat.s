54f60c9b81cbef8c6a952d826981a4e279aa0da2
GauriP19 <53228858+GauriP19@users.noreply.github.com>
2021-09-01 16:57:37 +0000

    Add files via upload

---
+++ AjitPublicResources/tools/isa_64_libraries/string/src/a64strcat.s
@@ -0,0 +1,80 @@
+.section ".text"
+.global a64strcat
+.type a64strcat, #function
+a64strcat:
+	mov 0x7,%l2
+	mov 0x7,%l3
+	anddcc %l2,%o0,%l2
+	bne b5
+
+begin64:mov 0x8, %l2
+	mov 0x8, %l3
+	mov %o0, %i0
+checkz:	ldd [%o0], %l0
+	zbytedpos %l0, 0xff, %l4
+	cmp %l4, 0x0
+	be,a checkz
+	add %o0, 0x8, %o0
+again64:ldd [%o1], %l0
+	addd %l2,%o0,%o0
+	zbytedpos %l0, 0xff, %l4
+	cmp %l4, 0x0
+	be,a again64
+	std %l0, [%o0 + -8]
+zcheck: srld %l0, 0x38, %l4
+	btst 0xff, %l5
+	be b8
+        srld %l0, 0x30, %l4
+        btst 0xff, %l5
+        be b7
+	nop
+        srld %l0, 0x28, %l4
+        btst 0xff, %l5
+        be b6
+	srld %l0, 0x30, %l4
+        srld %l0, 0x20, %l4
+        btst 0xff, %l5
+        be b5
+	st %l0, [%o0 + -8]
+        srld %l0, 0x18, %l4
+        btst 0xff, %l5
+        be b4
+        srld %l0, 0x10, %l4
+        btst 0xff, %l5
+        be b3
+	nop
+        srld %l0, 0x8, %l4
+        btst 0xff, %l5
+        be b2
+	srld %l0,0x10,%l4
+	btst 0xff, %l1
+	be b1
+	nop
+	
+b8:	clrb [%o0 + -8]
+	retl
+	mov %i0,%o0
+b7:	sth  %l5, [%o0 + -8]
+	retl
+	mov %i0,%o0
+b6:	sth  %l5, [%o0 + -8]
+	clrb [%o0 + -6]
+	retl
+	mov %i0,%o0
+b4:	clrb [%o0 + -4]
+	retl
+	mov %i0,%o0
+b3:	sth %l5, [%o0 + -4]
+	retl
+	mov %i0,%o0
+b2:	sth %l5, [%o0 + -4]
+	clrb [%o0 + -2]
+	retl
+	mov %i0,%o0
+b1: 	st %l1,[%o0 + -4]
+	retl
+	mov %i0,%o0
+b5:	retl
+	mov %i0,%o0
+
+
---
+++ AjitPublicResources/tools/isa_64_libraries/string/src/a64strcmp.s
@@ -0,0 +1,69 @@
+.section ".text"
+.global a64strcmp
+.type a64strcmp, #function
+a64strcmp: 
+
+	mov 0x7,%l2
+	mov 0x7,%l3
+	anddcc %l2,%o0,%l0
+	bne exit
+	nop
+begin64:mov 0x8, %l2
+	mov 0x8, %l3
+again64:ldd [%o0],%l0
+	ldd [%o1],%l6
+	subdcc %l0, %l6, %l4
+	bne uneq
+	zbytedpos %l0, 0xff, %l4
+	cmp %l4, 0x0 
+	be,a again64
+	addd %l2,%o0,%o0
+	mov 0, %o0
+exit:	retl
+	nop
+uneq: 	srld %l0, 0x30, %o2
+	srld %l6, 0x30, %o4
+	btst 0xff, %o3
+	be exit
+	subdcc %o3, %o5, %o0 
+	bne exit
+	srld %l0, 0x28, %o2
+        srld %l6, 0x28, %o4
+        btst 0xff, %o3
+        be exit
+        subdcc %o3, %o5, %o0
+        bne exit
+	srld %l0, 0x20, %o2
+        srld %l6, 0x20, %o4
+        btst 0xff, %o3
+        be exit
+        subdcc %o3, %o5, %o0
+        bne exit
+	srld %l0, 0x18, %o2
+        srld %l6, 0x18, %o4
+        btst 0xff, %o3
+        be exit
+        subdcc %o3, %o5, %o0
+        bne exit
+	srld %l0, 0x10, %o2
+        srld %l6, 0x10, %o4
+        btst 0xff, %o3
+        be exit
+        subdcc %o3, %o5, %o0
+        bne exit
+	srld %l0, 0x8, %o2
+        srld %l6, 0x8, %o4
+        btst 0xff, %o3
+        be exit
+        subdcc %o3, %o5, %o0
+        bne exit
+	btst 0xff, %l1
+	be exit
+	subdcc %l1, %l7, %o0
+	bne exit
+	nop
+
+
+
+	
+
