f8dbc016950df8eb8941a985dc5cd91a15ba2ce8
GauriP19 <53228858+GauriP19@users.noreply.github.com>
2021-09-08 11:48:37 +0530

    Add files via upload

---
+++ AjitPublicResources/tools/isa_64_libraries/string/include/a64string.h
@@ -0,0 +1,5 @@
+extern char *a64strcpy (char *dest, const char *src);
+extern char *a64strncpy (char *dest, const char *src, size_t n);
+extern char *a64strcat (char *dest, const char *src);
+extern int *a64strcmp (const char *dest, const char *src);
+extern int *a64strcasecmp (const char *dest, const char *src);
