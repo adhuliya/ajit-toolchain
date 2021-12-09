74931af70a7129a93a1eb6a5c4d62e97d37755a4
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2021-12-09 23:23:26 +0530

    [vishnu] Changes in Genereate and Vmlix script to output build in standalone build directory.          Created a build.sh for managingall scripts related to building the OS.          Created clean.sh script to remove build outputs.

--- os/Ajit_linux/GeneratememMapForAjit.sh
+++ os/Ajit_linux/GeneratememMapForAjit.sh
@@ -1,7 +1,13 @@
 
-BUILDROOT_BUILD_DIR=$PWD/buildroot-2014.08
-LINUX_BUILD_DIR=$PWD/buildroot-2014.08/output/build/linux-custom
-BUILDROOT_TAR=$PWD/buildroot-2014.08.tar.gz
+CWD=$PWD
+BUILD_DIR=$CWD/build
+BUILDROOT_TAR=$CWD/buildroot-2014.08.tar.gz
+BUILDROOT_BUILD_DIR=$BUILD_DIR/buildroot-2014.08
+DEVICE_TREE_BUILD_DIR=$BUILD_DIR/Ajit_device_tree
+BOOTLOADER_BUILD_DIR=$BUILD_DIR/Ajit_bootloader
+LINUX_BUILD_DIR=$BUILDROOT_BUILD_DIR/output/build/linux-custom
+BUSYBOX_BUILD_DIR=$BUILDROOT_BUILD_DIR/output/build/busybox-1.22.1
+
 
 # Check for presence of Argumentsand throw an error when there is none or invalid one.
 if [ "$#" -ne 1 ] || [ "$1" -lt 0 ] || [ "$1" -gt 3 ]; then
@@ -14,22 +20,42 @@ if [ "$#" -ne 1 ] || [ "$1" -lt 0 ] || [ "$1" -gt 3 ]; then
   exit 1
 fi
 
-#If you're running this script for the first time,
-#the destination folder may not exist.
-#Let's create it first.
+# If you're running this script for the first time,
+# the destination folders may not exist.
+# Let's create them first.
+
+if ! [ -d "$BUILD_DIR" ]; then
+  echo "================================================="
+  echo " STEP 0.1: "
+  echo " Creating folder build"
+  echo "================================================="
+  mkdir -p $BUILD_DIR
+fi
+
+# Copy device-tree and bootloader sources to build area.
+cp -r $CWD/Ajit_device_tree  $BUILD_DIR
+cp -r $CWD/Ajit_bootloader $BUILD_DIR
 
 if ! [ -d "$BUILDROOT_BUILD_DIR" ]; then
-  tar -xvf $BUILDROOT_TAR
+  tar -xvf $BUILDROOT_TAR --directory=$BUILD_DIR
   mkdir -p $BUILDROOT_BUILD_DIR/dl && \
-  cp br2-dl/* $BUILDROOT_BUILD_DIR/dl
+  cp $CWD/br2-dl/* $BUILDROOT_BUILD_DIR/dl
 fi
 
 if ! [ -d "$LINUX_BUILD_DIR" ]; then
   echo "================================================="
-  echo " STEP 0: "
-  echo " Creating folder buildroot-2014.08/output/build/linux-custom"
+  echo " STEP 0.2: "
+  echo " Creating folder build/buildroot-2014.08/output/build/linux-custom"
   echo "================================================="
   mkdir -p $LINUX_BUILD_DIR
+fi
+
+if ! [ -d "$BUSYBOX_BUILD_DIR" ]; then
+  echo "================================================="
+  echo " STEP 0.3: "
+  echo " Creating folder build/buildroot-2014.08/output/build/busybox-1.22.1"
+  echo "================================================="
+  mkdir -p $BUSYBOX_BUILD_DIR
 fi
 
 
@@ -40,23 +66,21 @@ echo "  from: ./Ajit_linux_3.16.1/"
 echo "  to  : ./buildroot-2014.08/output/build/linux-custom"
 echo "================================================="
 
-rsync -av --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS ./Ajit_linux_3.16.1/ ./buildroot-2014.08/output/build/linux-custom
+rsync -av --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS $CWD/Ajit_linux_3.16.1/ $LINUX_BUILD_DIR
 
+echo "================================================="
+echo " STEP 1.2: "
+echo "  copying Busybox source code (using rync)"
+echo "  from: ./Ajit_busybox-1.22.1/"
+echo "  to  : ./buildroot-2014.08/output/build/busybox-1.22.1"
+echo "================================================="
 
-# echo "================================================="
-# echo " STEP 1.2: "
-# echo "  copying Busybox source code (using rync)"
-# echo "  from: ./Ajit_busybox-1.22.1/"
-# echo "  to  : ./buildroot-2014.08/output/build/busybox-1.22.1"
-# echo "================================================="
-
-rsync -av --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS ./Ajit_busybox-1.22.1/ ./buildroot-2014.08/output/build/busybox-1.22.1
+rsync -av --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS $CWD/Ajit_busybox-1.22.1/ $BUSYBOX_BUILD_DIR
 
 
 echo " Running make clean inside linux build directory"
-cd ./buildroot-2014.08/output/build/linux-custom/
+cd $LINUX_BUILD_DIR
 make clean
-cd -
 
 
 echo "================================================="
@@ -64,21 +88,21 @@ echo " STEP 2: "
 echo "  Running 'make' inside buildroot folder."
 echo "================================================="
 
-cd buildroot-2014.08
+cd $BUILDROOT_BUILD_DIR
 
 if [ "$1" -eq 1 ]; then
   # Original defconfig with just the initial ramfs
   echo ""
   echo "****Defconfig: Ajit_defconfig****"
   echo ""
-  make BR2_EXTERNAL=$PWD/../Ajit_buildroot_configs Ajit_defconfig
+  make BR2_EXTERNAL=$CWD/Ajit_buildroot_configs Ajit_defconfig
 elif ! [ "$1" -eq 2]; then
   echo ""
   echo "****Defconfig: Ajit_rootfs_$1_defconfig****"
   echo ""
-  make BR2_EXTERNAL=$PWD/../Ajit_buildroot_configs Ajit_rootfs_$1_defconfig
+  make BR2_EXTERNAL=$CWD/Ajit_buildroot_configs Ajit_rootfs_$1_defconfig
   make && \
-  cp ./output/images/rootfs.tar ./../Ajit_buildroot_configs/board/overlay/initramfs
+  cp $BUILDROOT_BUILD_DIR/output/images/rootfs.tar $CWD/Ajit_buildroot_configs/board/overlay/initramfs
 
   # building initramfs with overlay that has the above rootfs, 
   # kernel, and then combining both to a single binary
@@ -86,17 +110,16 @@ elif ! [ "$1" -eq 2]; then
   echo ""
   echo "****Defconfig: Ajit_rootfs_$1_defconfig****"
   echo ""
-  make BR2_EXTERNAL=$PWD/../Ajit_buildroot_configs Ajit_initramfs_$1_defconfig
+  make BR2_EXTERNAL=$CWD/Ajit_buildroot_configs Ajit_initramfs_$1_defconfig
 fi
 
-make && \
-cd -
+make
 
 echo "================================================="
 echo " STEP 3: "
 echo "  Compiling the device tree."
 echo "================================================="
-cd ./Ajit_device_tree/
+cd $DEVICE_TREE_BUILD_DIR
 echo "Checking if Device tree compiler (dtc) is installed ..."
 command -v dtc
 if [ $? -eq 0 ]
@@ -108,17 +131,14 @@ else
   echo "Please install dtc, preferably version 1.4.0"
   echo "For now, using the existing pre-compiled file Ajit_device_tree.s for generating bootloader image"
 fi
-cd -
 
 
 echo "================================================="
 echo " STEP 4: "
 echo "  Compiling bootloader code."
 echo "================================================="
-cd ./Ajit_bootloader/
-./compileBootloader.py AjitBootloader.c ../Ajit_device_tree/Ajit_device_tree.s
-cd -
-
+cd $CWD/Ajit_bootloader/
+./compileBootloader.py AjitBootloader.c $CWD/Ajit_device_tree/Ajit_device_tree.s
 
 echo "================================================="
 echo " STEP 5: "
@@ -126,24 +146,23 @@ echo "  Combining the bootloader executable and the kernel image "
 echo "  into a single memory-map"
 echo "================================================="
 
-if [[ ! -d Generated_memory_map ]]
+if [[ ! -d $BUILD_DIR/Generated_memory_map ]]
 then
-  mkdir Generated_memory_map
+  mkdir $BUILD_DIR/Generated_memory_map
 fi
-cp Ajit_bootloader/output/AjitBootloader.elf ./Generated_memory_map/
-cp Ajit_bootloader/output/AjitBootloader_*.txt ./Generated_memory_map/
-cp buildroot-2014.08/output/images/vmlinux ./Generated_memory_map/
+cp $BOOTLOADER_BUILD_DIR/output/AjitBootloader.elf $BUILD_DIR/Generated_memory_map/
+cp $BOOTLOADER_BUILD_DIR/output/AjitBootloader_*.txt $BUILD_DIR/Generated_memory_map/
+cp $BUILDROOT_BUILD_DIR/output/images/vmlinux $BUILD_DIR/Generated_memory_map/
 
 
-cd ./Generated_memory_map/
-vmlinuxToMemmap.py AjitBootloader.elf vmlinux 
-cd -
+cd $BUILD_DIR/Generated_memory_map/
+vmlinuxToMemmap.py AjitBootloader.elf vmlinux
+
+cd $CWD
 
 echo "================================================="
 echo " "
 echo "  If there were no errors, the memory map will be created in "
-echo "  ./Generated_memory_map/Output/mem_map.txt"
+echo "  build/Generated_memory_map/Output/mem_map.txt"
 echo " "
 echo "================================================="
-
-
--- os/Ajit_linux/vmLinuzToMemmapAtf0004000.sh
+++ os/Ajit_linux/vmLinuzToMemmapAtf0004000.sh
@@ -1,15 +1,20 @@
 # Script assumes that kernel has been built in buildroot.
+CWD=$PWD
+BUILD_DIR=$CWD/build
+BUILDROOT_BUILD_DIR=$BUILD_DIR/buildroot-2014.08
+DEVICE_TREE_BUILD_DIR=$BUILD_DIR/Ajit_device_tree
+BOOTLOADER_BUILD_DIR=$BUILD_DIR/Ajit_bootloader
 
 # remove old stuff and make it again.
-rm -rf Generated_memory_map.f0004000
-mkdir Generated_memory_map.f0004000
+rm -rf $BUILD_DIR/Generated_memory_map.f0004000
+mkdir -p $BUILD_DIR/Generated_memory_map.f0004000
 
 echo "================================================="
 echo " STEP 0: "
 echo "  Compiling bootloader code. starts at 0xf0000000"
 echo "================================================="
-cd ./Ajit_bootloader/
-./compileBootloader.f0000000.py AjitBootloader.c ../Ajit_device_tree/Ajit_device_tree.s
+cd $BOOTLOADER_BUILD_DIR
+./compileBootloader.f0000000.py AjitBootloader.c $CWD/Ajit_device_tree/Ajit_device_tree.s
 cd -
 
 
@@ -18,12 +23,10 @@ echo " STEP 1: "
 echo "  Combining the bootloader executable and the kernel image "
 echo "  into a single memory-map"
 echo "================================================="
-cp Ajit_bootloader/output.f0000000/AjitBootloader.elf   ./Generated_memory_map.f0004000/
-cp Ajit_bootloader/output.f0000000/AjitBootloader_*.txt ./Generated_memory_map.f0004000/
-cp buildroot-2014.08/output/images/vmlinux ./Generated_memory_map.f0004000/
+cp $BOOTLOADER_BUILD_DIR/output.f0000000/AjitBootloader.elf   $BUILD_DIR/Generated_memory_map.f0004000/
+cp $BOOTLOADER_BUILD_DIR/output.f0000000/AjitBootloader_*.txt $BUILD_DIR/Generated_memory_map.f0004000/
+cp $BUILDROOT_BUILD_DIR/output/images/vmlinux $BUILD_DIR/Generated_memory_map.f0004000/
 
-cd ./Generated_memory_map.f0004000/
+cd $BUILD_DIR/Generated_memory_map.f0004000/
 vmlinuxToMemmapRevised.py AjitBootloader.elf vmlinux 0xf0004000 -o Output.f0004000 
-cd -
-
-
+cd $CWD
