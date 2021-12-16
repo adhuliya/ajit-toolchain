5409443bccfa702b6c4b0f0ccc989767db3a5f3b
Vishnu Easwaran E <easwaranvishnu@gmail.com>
2021-12-16 10:45:46 +0530

    [cishnu] Updated clean.sh for removing overlays from rootfs sources too.

--- os/Ajit_linux/clean.sh
+++ os/Ajit_linux/clean.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Remove 
+#       - contents of "build" drirectory
+#       - overlay contents from rootfs
+
 rm -rf build/
+rm -rf $AJIT_LINUX_DIR/Ajit_buildroot_configs/board/rootfs_overlay/home/Ajit_overlay
+rm -rf $AJIT_LINUX_DIR/Ajit_buildroot_configs/board/overlay/rootfs/home/Ajit_overlay
