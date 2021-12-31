94b70da1f8cf1ddf9798e9208a7e970605949e90
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-12-31 16:23:18 +0530

    Basic infrastructure for generic handling of interrupts and software traps in place. interrupt levels 1-15, and sw traps from 0-15 are in place.

---
+++ validation_ladder/basic_tests/sw_traps/generic_sw_trap_test/init.s
@@ -0,0 +1,36 @@
+.section .text.ajitstart
+.global _start;
+_start:
+	call clear_stack_pointers
+	nop
+
+	set 0xfff00f00, %sp
+	set 0xfff00ff0, %fp
+
+	set 0x1, %l0		! window 7 is marked invalid...  we start at window 7
+	sll %l0, 7, %l0         !
+	wr %l0, 0x0, %wim	!
+
+	! trap table.
+	set	trap_table_base, %l0
+	wr	%l0, 0x0, %tbr
+
+	! set up virtual -> physical map.
+	call page_table_setup 	
+	nop
+
+	call set_context_table_pointer 	
+	nop
+
+  	! enable mmu.
+	set 0x1, %o0
+	sta %o0, [%g0] 0x4    
+
+	! enable traps, go to user mode.
+	set 0x1067, %l0	
+	wr %l0, %psr
+
+	call main
+	nop
+
+	ta 0
---
+++ validation_ladder/basic_tests/sw_traps/generic_sw_trap_test/main.c
@@ -0,0 +1,59 @@
+//Program for RPN calculator  using serial sw_traps
+#include <stdint.h>
+#include <stdio.h>
+#include <setjmp.h>
+#include "ajit_access_routines.h"
+#include "core_portme.h"
+#include "ajit_mt_sw_traps.h"
+
+#define NWORDS 16
+#define PRINTF ee_printf
+
+int exit_flag = 0;
+
+int __enable_serial()
+{
+	__ajit_write_serial_control_register_via_vmap__ ( TX_ENABLE | RX_ENABLE);
+	return(0);
+}
+
+int __enable_serial_sw_trap()
+{
+	__ajit_write_serial_control_register_via_vmap__ (TX_ENABLE | RX_ENABLE |RX_INTR_ENABLE);
+	return(0);
+}
+
+
+void __t5_handler(uint32_t arg1, uint32_t arg2, uint32_t arg3)
+{
+	*((uint32_t*) arg2) = __ajit_load_word_from_physical_address__((uint64_t) arg1);
+	PRINTF("in __t5_handler(0x%x, 0x%x, 0x%x) = 0x%x\n", arg1, arg2, arg3, *((uint32_t*) arg2));
+}
+
+
+void read_physical_memory (uint32_t address, uint32_t value_pointer)
+{
+	__AJIT_SW_TRAP_5();
+}
+
+
+int main () 
+{
+	__enable_serial();
+
+	// set up software trap handler.
+	ajit_initialize_sw_trap_handlers_to_null();
+	ajit_set_sw_trap_handler(5, &(__t5_handler));	
+
+	uint32_t v; 
+
+	uint32_t index;
+	for(index = 0x0; index < NWORDS; index++)
+	{
+		read_physical_memory (0x40000000 + (index<<2), (uint32_t) &v);
+		PRINTF("0x%x = PMEM[0x%x]\n", v, 0x40000000 + (index << 2));
+	}
+
+
+	return(0);
+}
