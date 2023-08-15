c32112d09969a295960a39dcc99a4a109676e21a
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2023-08-15 12:17:38 +0530

    Added comment in string a64 include file.

--- AjitPublicResources/tools/isa_64_libraries/string/include/a64string.h
+++ AjitPublicResources/tools/isa_64_libraries/string/include/a64string.h
@@ -1,5 +1,11 @@
+#ifndef a64string_h____
+#define a64string_h____
+
+// TODO: this is out of data..
 extern char *a64strcpy (char *dest, const char *src);
 extern char *a64strncpy (char *dest, const char *src, size_t n);
 extern char *a64strcat (char *dest, const char *src);
 extern int *a64strcmp (const char *dest, const char *src);
 extern int *a64strcasecmp (const char *dest, const char *src);
+
+#endif
