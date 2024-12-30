d0da85293015de78b100883ca3c54c826656f9ca
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2024-12-30 16:08:14 +0530

    Added missing file.

---
+++ AjitPublicResources/processor/64bit/C_multi_core_multi_thread/swizzler/include/measures.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#ifndef measures_h___
+#define  measures_h___
+double measure1(const char* inp);
+double measure2(const char* inp1, const char* inp2);
+#endif
